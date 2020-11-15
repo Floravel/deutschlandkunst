9c26a46aaf5868779d106f30e43691942c872c84
floravel <flo-hess1@web.de>
2020-11-15 09:16:47 +0100

    set up css in project

---
+++ resources/js/components/Posts/Index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <table class="table">
+            <thead>
+            <tr>
+                <th>Title</th>
+                <th>Post text</th>
+                <th>Created date</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Something</td>
+                <td>Longer Text</td>
+                <td>2020-11-18</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Something</td>
+                <td>Longer Text</td>
+                <td>2020-11-23</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Something</td>
+                <td>Longer Text</td>
+                <td>2020-11-28</td>
+                <td></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
